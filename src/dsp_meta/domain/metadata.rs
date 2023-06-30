055242748fdb06847459aaba72f5bf7fbea6b6a1
Ivan Subotic <400790+subotic@users.noreply.github.com>
2023-06-30 19:49:06 +0200

    add ci (#20)

--- src/dsp_meta/domain/metadata.rs
+++ src/dsp_meta/domain/metadata.rs
@@ -16,9 +16,9 @@ pub struct Metadata {
 impl Metadata {
     pub fn make(project: Project, datasets: Vec<Dataset>, persons: Vec<Person>) -> Metadata {
         Metadata {
-            project: project,
-            datasets: datasets,
-            persons: persons,
+            project,
+            datasets,
+            persons,
         }
     }
 }
