d7bbe2eb135e53af7e883d4f1af46a7883002397
Ivan Subotic <400790+subotic@users.noreply.github.com>
2023-10-03 16:04:22 +0200

    configure logging

--- src/dsp_meta/api/app.rs
+++ src/dsp_meta/api/app.rs
@@ -2,14 +2,13 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use axum::body::Bytes;
-use axum::extract::MatchedPath;
 use axum::http::{HeaderMap, Request};
 use axum::response::Response;
 use axum::routing::get;
 use axum::Router;
 use tower_http::classify::ServerErrorsFailureClass;
 use tower_http::trace::TraceLayer;
-use tracing::{info_span, Span};
+use tracing::{info_span, trace, Span};
 
 use crate::api::handler::{health, project_metadata_handler};
 use crate::app_state::AppState;
@@ -29,49 +28,31 @@ pub fn app(shared_state: Arc<AppState>) -> Router {
         )
         .route("/health", get(health::health_handler))
         .with_state(shared_state)
-        // `TraceLayer` is provided by tower-http so you have to add that as a dependency.
-        // It provides good defaults but is also very customizable.
-        //
-        // See https://docs.rs/tower-http/0.1.1/tower_http/trace/index.html for more details.
-        //
-        // If you want to customize the behavior using closures here is how.
+        // See https://docs.rs/tower-http/latest/tower_http/trace/index.html for more details
+        // on how to customize.
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(|request: &Request<_>| {
-                    // Log the matched route's path (with placeholders not filled in).
-                    // Use request.uri() or OriginalUri if you want the real path.
-                    let matched_path = request
-                        .extensions()
-                        .get::<MatchedPath>()
-                        .map(MatchedPath::as_str);
-
                     info_span!(
                         "http_request",
                         method = ?request.method(),
-                        matched_path,
-                        some_other_field = tracing::field::Empty,
+                        uri = request.uri().to_string(),
+                        status_code = tracing::field::Empty,
+                        latency = tracing::field::Empty,
                     )
                 })
-                .on_request(|_request: &Request<_>, _span: &Span| {
-                    // You can use `_span.record("some_other_field", value)` in one of these
-                    // closures to attach a value to the initially empty field in the info_span
-                    // created above.
+                .on_request(|_request: &Request<_>, _span: &Span| ())
+                .on_response(|response: &Response, latency: Duration, span: &Span| {
+                    span.record("status_code", response.status().as_u16());
+                    span.record("latency", latency.as_micros());
+                    trace!("response handled by server");
                 })
-                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
-                    // ...
-                })
-                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
-                    // ...
-                })
+                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| ())
                 .on_eos(
-                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
-                        // ...
-                    },
+                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| (),
                 )
                 .on_failure(
-                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
-                        // ...
-                    },
+                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| (),
                 ),
         )
 }
