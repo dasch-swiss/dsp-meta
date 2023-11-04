8657e69899efe36c970287b1de9a59ebe21e4b6c
Ivan Subotic <400790+subotic@users.noreply.github.com>
2023-11-04 19:40:51 +0100

    feat: serialize project to rdf (ongoing)

--- src/dsp_meta/api/convert/rdf/constance.rs
+++ src/dsp_meta/api/convert/rdf/constance.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 pub const RDF_NAMESPACE_STRING: &str = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 pub const RDFS_NAMESPACE_STRING: &str = "http://www.w3.org/2000/01/rdf-schema#";
 pub const XSD_NAMESPACE_STRING: &str = "http://www.w3.org/2001/XMLSchema#";
--- src/dsp_meta/api/convert/rdf/triple.rs
+++ src/dsp_meta/api/convert/rdf/triple.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 struct Triple {
     subject: String,
     predicate: String,
