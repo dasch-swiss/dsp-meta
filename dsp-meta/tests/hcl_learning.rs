59dd116f59d3c0c577165ef2c43c957ff4449887
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-07-18 15:16:43 +0200

    add keywords

--- dsp-meta/tests/hcl_learning.rs
+++ dsp-meta/tests/hcl_learning.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use hcl::Body;
 use serde::Deserialize;
 
@@ -29,10 +31,15 @@ pub struct ProjectMetadata {
     version: Version,
     discipline: Discipline,
     user: User,
+    #[serde(rename = "keyword")]
+    keywords: Vec<Keyword>,
 }
 
 #[derive(Deserialize, Debug, PartialEq)]
 struct Version(usize);
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct Keyword(HashMap<String, String>);
 
 #[test]
 fn test_hcl() {
@@ -48,11 +55,19 @@ fn test_hcl() {
             name = "John Doe"
             email = "john@doe.tld"
         }
+        keyword {
+          en ="word"
+          de = "wort"
+        }
+        keyword {
+          en ="word2"
+        }
     "#;
 
     let body: Body = hcl::from_str(input).expect("Failed to parse");
 
     let metadata: ProjectMetadata = hcl::from_body(body).expect("Failed to parse");
+
     let exp = Discipline {
         skos: Some(Skos(RefData {
             ref_id: "foo".to_string(),
@@ -61,11 +76,27 @@ fn test_hcl() {
             ref_id: "bar".to_string(),
         })),
     };
+
+    let mut exp_keywords = Vec::new();
+    let keyw1 = Keyword({
+        let mut map = HashMap::new();
+        map.insert("en".to_string(), "word".to_string());
+        map.insert("de".to_string(), "wort".to_string());
+        map
+    });
+    let kew2 = Keyword({
+        let mut map = HashMap::new();
+        map.insert("en".to_string(), "word2".to_string());
+        map
+    });
+    exp_keywords.append(&mut vec![keyw1, kew2]);
+
     assert_eq!(
         metadata,
         ProjectMetadata {
             version: Version(1),
             discipline: exp,
+            keywords: exp_keywords,
             user: User {
                 name: "John Doe".to_string(),
                 email: "john@doe.tld".to_string()
