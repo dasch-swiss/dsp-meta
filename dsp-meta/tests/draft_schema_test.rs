2188989252c0e24b2ee7fc0db6b9cf45db1687a9
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-07-24 16:23:18 +0200

    fix tests

--- dsp-meta/tests/draft_schema_test.rs
+++ dsp-meta/tests/draft_schema_test.rs
@@ -7,30 +7,31 @@ use dsp_meta::api::convert::serde::json_schema_validator::{validate_files, Schem
 
 #[test]
 fn test_json_and_yaml_serialization_are_equal() {
-    let path = "/Users/christian/git/dasch/dsp-meta/data/examples/sgv.json";
+    let mut path = data_dir();
+    path.push("json");
+    path.push("sgv.json");
     let contents_json = fs::read_to_string(path).expect("Read JSON");
     let metadata_json = serde_json::from_str::<DraftMetadata>(&*contents_json).expect("From JSON");
-    let contents_yaml =
-        fs::read_to_string("/Users/christian/git/dasch/dsp-meta/data/examples/sgv.yaml")
-            .expect("Read YML");
+    let contents_yaml = serde_yaml::to_string(&metadata_json).expect("To YAML");
     let metadata_yaml = serde_yaml::from_str(&*contents_yaml).expect("From YAML");
     assert_eq!(metadata_json, metadata_yaml);
 }
 
 #[test]
 fn test_json_and_toml_serialization_are_equal() {
-    let path = "/Users/christian/git/dasch/dsp-meta/data/examples/sgv.json";
+    let mut path = data_dir();
+    path.push("json");
+    path.push("sgv.json");
+    println!("{:?}", path);
     let contents_json = fs::read_to_string(path).expect("Read JSON");
     let metadata_json = serde_json::from_str::<DraftMetadata>(&*contents_json).expect("From JSON");
-    let contents_toml =
-        fs::read_to_string("/Users/christian/git/dasch/dsp-meta/data/examples/sgv.toml")
-            .expect("Read TOML");
+    let contents_toml = toml::to_string(&metadata_json).expect("To TOML");
     let metadata_toml = toml::from_str::<DraftMetadata>(&*contents_toml).expect("From TOML");
     assert_eq!(metadata_json, metadata_toml);
 }
 
 #[test]
-fn test_deserialization_data() {
+fn test_deserialization_all_json_data() {
     let paths = collect_data_json_paths();
     let mut success: usize = 0;
     let mut error: usize = 0;
@@ -60,11 +61,7 @@ fn test_deserialization_data() {
 }
 
 fn collect_data_json_paths() -> Vec<PathBuf> {
-    let mut current_dir = env::current_dir()
-        .ok()
-        .and_then(|e| e.parent().map(|p| p.to_path_buf()))
-        .expect("Project root dir");
-    current_dir.push("data");
+    let mut current_dir = data_dir();
     current_dir.push("json");
     fs::read_dir(current_dir)
         .expect("Failed to read data_dir")
@@ -78,6 +75,15 @@ fn collect_data_json_paths() -> Vec<PathBuf> {
             }
         })
         .collect::<Vec<PathBuf>>()
+}
+
+fn data_dir() -> PathBuf {
+    let mut current_dir = env::current_dir()
+        .ok()
+        .and_then(|e| e.parent().map(|p| p.to_path_buf()))
+        .expect("Project root dir");
+    current_dir.push("data");
+    current_dir
 }
 
 #[test]
