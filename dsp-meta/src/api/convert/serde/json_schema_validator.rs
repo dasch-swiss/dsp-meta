20e9e8686d5bb88ca5e12098ef47ba6262aac45c
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-07-25 08:12:45 +0200

    derive Error for ValidationError

--- dsp-meta/src/api/convert/serde/json_schema_validator.rs
+++ dsp-meta/src/api/convert/serde/json_schema_validator.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::path::Path;
 
 use serde_json::Value;
+use thiserror::Error;
 use valico::json_schema::schema::ScopedSchema;
 use valico::json_schema::{Scope, ValidationState};
 
@@ -23,10 +24,13 @@ impl SchemaVersion {
 }
 
 pub type Result<T> = core::result::Result<T, ValidationError>;
-#[derive(Debug)]
+#[derive(Debug, Error )]
 pub enum ValidationError {
+    #[error("File not loaded: {0}")]
     FileNotLoaded(std::io::Error),
+    #[error("Schema is invalid: {0}")]
     SchemaError(valico::json_schema::schema::SchemaError),
+    #[error("Error parsing file as json: {0}")]
     NotAJsonFile(serde_json::Error),
 }
 
