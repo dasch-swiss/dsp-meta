fea4c7d9675dd68f391dd3aa81d57c57f4425931
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-07-29 12:52:09 +0200

    make snackbar easier on the eyes with fading in and out a bit slower

--- web-frontend/src/projects-repository/ProjectsRepository.svelte
+++ web-frontend/src/projects-repository/ProjectsRepository.svelte
@@ -51,7 +51,7 @@
   </div>
 {/if}
 
-<main in:fade="{{duration: 200}}">
+<main transition:fade={{duration: 400}}>
   <div class=tile-container>
     {#if $pagedResults && $pagedResults.length}
       {#each $pagedResults as project}
