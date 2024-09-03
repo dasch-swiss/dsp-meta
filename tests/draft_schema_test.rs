000a7c2b9a141df4514bfff775d47a82dd753cd1
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-09-03 10:15:39 +0200

    rm unused dependencies

--- tests/draft_schema_test.rs
+++ tests/draft_schema_test.rs
@@ -7,29 +7,6 @@ use dsp_meta::domain::model::draft_model::*;
 use dsp_meta::domain::model::json_schema_validator::{validate_files, SchemaVersion};
 use dsp_meta::infrastructure::load_json_file_paths;
 
-#[test]
-fn test_json_and_yaml_serialization_are_equal() {
-    let mut sgv_json = data_dir();
-    sgv_json.push("json");
-    sgv_json.push("sgv.json");
-    let contents_json = fs::read_to_string(sgv_json).expect("Read JSON");
-    let metadata_json = serde_json::from_str::<DraftMetadata>(&*contents_json).expect("From JSON");
-    let contents_yaml = serde_yaml::to_string(&metadata_json).expect("To YAML");
-    let metadata_yaml = serde_yaml::from_str(&*contents_yaml).expect("From YAML");
-    assert_eq!(metadata_json, metadata_yaml);
-}
-
-#[test]
-fn test_json_and_toml_serialization_are_equal() {
-    let mut sgv_json = data_dir();
-    sgv_json.push("json");
-    sgv_json.push("sgv.json");
-    let contents_json = fs::read_to_string(sgv_json).expect("Read JSON");
-    let metadata_json = serde_json::from_str::<DraftMetadata>(&*contents_json).expect("From JSON");
-    let contents_toml = toml::to_string(&metadata_json).expect("To TOML");
-    let metadata_toml = toml::from_str::<DraftMetadata>(&*contents_toml).expect("From TOML");
-    assert_eq!(metadata_json, metadata_toml);
-}
 
 #[test]
 fn test_deserialization_all_json_data() {
